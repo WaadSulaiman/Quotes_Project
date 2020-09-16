475fbe091df12152d2abb1caf128072f6f8b9fa8
WaadSulaiman <waad.sulaiman@outlook.com>
2020-09-16 17:51:24 +0200

    The activity issuse is resolved and the recycler view is now working.

--- app/src/main/java/com/example/quotes/activitys/CurrentCategory.kt
+++ app/src/main/java/com/example/quotes/activitys/CurrentCategory.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
-import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.quotes.R
 import com.example.quotes.TopSpacingDecoration
@@ -13,10 +13,10 @@ import com.example.quotes.adapters.CategoryAdapter
 import com.example.quotes.models.QuoteModel
 import kotlinx.android.synthetic.main.activity_current_category.*
 import org.json.JSONException
-import org.json.JSONObject
 
 class CurrentCategory : AppCompatActivity() {
     private val dataQuotes: MutableList<QuoteModel> = mutableListOf()
+    private var url: String? = null
     private lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,42 +24,40 @@ class CurrentCategory : AppCompatActivity() {
         setContentView(R.layout.activity_current_category)
         Log.d("URL", "hello")
         layout_current_category_textView_title.text = intent.getStringExtra("Category")
-
-        layout_current_category_recycler_view.apply {
-            fetchData(intent.getStringExtra("url")!!)
-            layoutManager = LinearLayoutManager(context)
+        url = intent.getStringExtra("url")
+        //adapter = CategoryAdapter(dataQuotes)
+        current_category_recycler_view.apply {
+            loadData()
+            adapter = CategoryAdapter(dataQuotes)
+            layoutManager = LinearLayoutManager(this@CurrentCategory)
             addItemDecoration(TopSpacingDecoration(30))
-            adapter = adapter
         }
 
     }
 
-    private fun fetchData(url: String) {
-        Log.d("testt", "test")
-        val request: JsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null, {
+    private fun loadData() {
+        dataQuotes.clear()
+        val request = JsonObjectRequest(Request.Method.GET, url, null, {
             try {
-                dataQuotes.clear()
-                for (jsonObjectIndex in 0 until it.length()) {
-                    val currentJsonObject: JSONObject = it.getJSONObject(jsonObjectIndex)
-                    val quote: QuoteModel = QuoteModel(
-                        author = currentJsonObject.getString("Author"),
-                        quoteText = currentJsonObject.getString("Quotes")
+                val quotesArray = it.getJSONArray("Quotes")
+                for (index in 0 until quotesArray.length()) {
+                    val currentObject = quotesArray.getJSONObject(index)
+                    val quote = QuoteModel(
+                        currentObject.getString("Author"),
+                        currentObject.getString("Quotes")
                     )
-                    //Log.d("testt", currentJsonObject.getString("Author"))
                     dataQuotes.add(quote)
                 }
-                dataQuotes.shuffle()
-                adapter = CategoryAdapter(dataQuotes)
-                layout_current_category_recycler_view.adapter = adapter
-
             } catch (event: JSONException) {
-                Log.d("eventJson", event.message)
             }
+            adapter = CategoryAdapter(dataQuotes)
+            current_category_recycler_view.adapter = adapter
+
         }, {
-            {
-            }
+
         }
         )
         Volley.newRequestQueue(this).add(request)
     }
+
 }
